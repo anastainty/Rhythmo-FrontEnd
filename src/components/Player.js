b71455c81ded2cd845cc3b2aec6610a188c38bbf
iryna <ir.tukay@gmail.com>
2024-10-10 20:59:41 +0300

    background picture added

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,10 +7,10 @@ const Player = () => {
       <div className="player-info">
         <p>Now playing: Song Title</p>
       </div>
-        <audio className="player-range" id="audioPlayer" controls>
-            <source src="Likearec.mp3" type="audio/mp3"/>
-            Your browser does not support the audio element.
-        </audio>
+      <audio className="player-range" id="audioPlayer" controls>
+        <source src="/song.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   );
 };
