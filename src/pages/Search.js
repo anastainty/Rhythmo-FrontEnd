007fcacc0c3c1b2470f66495d8c212e5932cc03a
iryna <ir.tukay@gmail.com>
2024-10-12 20:57:05 +0300

    genres for search with colors

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -10,7 +10,15 @@ const Search = () => {
   const searchOptions = ['All', 'Playlists', 'Podcasts', 'Songs', 'Albums', 'Artists', 'Profiles'];
 
 
-  const genres = ['Pop', 'Rock', 'Hip-Hop', 'Indie Rock', 'Classical', 'Metal'];
+  const genres = [
+    { name: 'Pop', gradient: 'linear-gradient(to bottom, rgb(225, 154, 176), rgb(156, 197, 197))', color: 'rgb(145, 94, 101)' }, // Вертикальный градиент для попа
+    { name: 'Rock', gradient: 'linear-gradient(to bottom, rgb(166, 67, 56), rgb(203, 130, 88))', color:'rgb(106, 41, 41)'}, // Вертикальный градиент для рока
+    { name: 'Hip-Hop', gradient: 'linear-gradient(to bottom, rgb(132, 132, 210), rgb(60, 32, 181))', color:'rgb(33, 19, 95)' }, // Вертикальный градиент для хип-хопа
+    { name: 'Indie Rock', gradient: 'linear-gradient(to bottom, rgb(132, 210, 187), rgb(52, 164, 93))', color:'rgb(34, 110, 62)' }, // Вертикальный градиент для инди
+    { name: 'Classical', gradient: 'linear-gradient(to bottom, rgb(166, 145, 52), rgb(217, 238, 133))', color: 'rgb(144, 104, 62)' }, // Вертикальный градиент для классики
+    { name: 'Metal', gradient: 'linear-gradient(to bottom, rgb(144, 92, 62), rgb(139, 39, 23)', color: 'rgb(255, 124, 76)' }, // Вертикальный градиент для метала
+  ];
+
 
   // Обработчик изменений поискового ввода
   const handleSearchChange = (e) => {
@@ -49,9 +57,13 @@ const Search = () => {
       {/* Жанры */}
       <h4 className="genres-heading">Genres&nbsp;&nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;&nbsp;Discover new music</h4>      // &nbsp;&nbsp; - это пробелы
       <div className="genres-container">
-        {genres.map((genre) => (
-          <div key={genre} className="genre-box">
-            {genre}
+      {genres.map((genre) => (
+          <div
+            key={genre.name}
+            className="genre-box"
+            style={{ background: genre.gradient, color: genre.color }} // Устанавливаем градиент как инлайн-стиль
+          >
+            {genre.name}
           </div>
         ))}
       </div>
