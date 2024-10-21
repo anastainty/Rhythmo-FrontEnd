60d18582791aaaf40083bd2468b2a4b6271786bd
ulanaobuhovic <obukhovich_04@mail.ru>
2024-10-21 11:28:34 +0300

    changed the layout of the registration and login pages

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Home from './pages/Home';
 import Library from './pages/Library';
 import Search from './pages/Search';
 import Profile from './pages/Profile';
-import PageTitle from './components/PageTitle'; // Импортируйте PageTitle
+import PageTitle from './components/PageTitle';
 import Registration from './pages/Registration';
 import './App.scss';
 
 const App = () => {
+  const location = useLocation();
+  const isRegistrationPage = location.pathname === '/registration';
+
   return (
-    <Router>
-      <div className="app-container">
-        {/* Боковая панель навигации */}
-        <Sidebar />
-
-        {/* Основной контент */}
-        <div className="content-container">
+    <div className="app-container">
+      {!isRegistrationPage && <Sidebar />}
+      <div
+        className={`content-container ${isRegistrationPage ? 'registration-style' : ''}`}
+      >
+        {!isRegistrationPage && (
           <div className="profile-header">
-            <PageTitle /> {/* Заголовок страницы */}
-            <li> <Link to = "/registration" className="logout-button"> Log out</Link></li>
+            <PageTitle />
+            <li>
+              <Link to="/registration" className="logout-button">Log out</Link>
+            </li>
             <Link to="/pages/profile">
-            <img src="/profile-picture.jpg" alt="Profile" className="profile-picture" />
+              <img src="/profile-picture.jpg" alt="Profile" className="profile-picture" />
             </Link>
-
           </div>
+        )}
 
-          <Routes>
-            <Route path='/pages/profile' element={<Profile />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/registration" element={<Registration />} />
-          </Routes>
-        </div>
-
-        {/* Плеер внизу */}
-        <Player />
+        <Routes>
+          <Route path="/pages/profile" element={<Profile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
       </div>
-    </Router>
+
+      {!isRegistrationPage && <Player />}
+    </div>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
